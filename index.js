440618792df7ff798d5f9af102e22c296cfa52b9
Matheus Mitestainer <matheusmitestainer@outlook.com>
2020-11-22 23:07:42 -0300

    Add clock logic

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-document.onload(() => {
-    const days = document.getElementById('days')
-    const hours = document.getElementById('hours')
-    const minutes = document.getElementById('minutes')
-    const seconds = document.getElementById('seconds')
+const days = document.getElementById('days')
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
 
-    const date = new Date()
-    const now = new Date().now
-})
+window.onload = () => {
+
+    setInterval(() => {
+        const date = new Date('Nov 26 2020 20:30').getTime()
+        const now = new Date().getTime()
+    
+        const difference = (date - now)
+    
+        days.innerText = Math.floor(difference / (1000 * 60 * 60 * 24))
+        hours.innerText = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        minutes.innerText = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+        seconds.innerText = Math.floor((difference % (1000 * 60)) / 1000)
+    }, 1000);
+
+}
+
+
+
+
