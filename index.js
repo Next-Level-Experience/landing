901e94825f8a5fe634abcfd19ebf7d7dbe824152
Matheus Mitestainer <matheusmitestainer@outlook.com>
2020-11-22 22:22:26 -0300

    Add files

---
+++ index.js
@@ -0,0 +1,9 @@
+document.onload(() => {
+    const days = document.getElementById('days')
+    const hours = document.getElementById('hours')
+    const minutes = document.getElementById('minutes')
+    const seconds = document.getElementById('seconds')
+
+    const date = new Date()
+    const now = new Date().now
+})
